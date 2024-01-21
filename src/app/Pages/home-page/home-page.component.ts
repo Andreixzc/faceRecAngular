502d1e4f0ceff459a450282ae11759c0780af592
Andrei <andrei.massaini@hotmail.com>
2024-01-21 20:07:48 -0300

    create account redirect

--- src/app/Pages/home-page/home-page.component.ts
+++ src/app/Pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../Components/navbar/navbar.component';
 import { FooterComponent } from '../../Components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent],
+  imports: [NavbarComponent,FooterComponent,RouterModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
