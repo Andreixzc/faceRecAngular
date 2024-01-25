9e7d42d1949e40baff419c2737a2b29096eb2682
Andrei <andrei.massaini@hotmail.com>
2024-01-25 02:09:23 -0300

    Adding login page template

--- src/app/Pages/login-page/login-page.component.ts
+++ src/app/Pages/login-page/login-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../Components/navbar/navbar.component';
+import { FooterComponent } from '../../Components/footer/footer.component';
+import { LoginComponent } from '../../Components/login/login.component';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent,FooterComponent,LoginComponent],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
