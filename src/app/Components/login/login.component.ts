950b9c149923e5efc25820dab00ad0a5e3719506
Andrei <andrei.massaini@hotmail.com>
2024-01-25 02:16:31 -0300

    Adding formgroup to login form

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -10,12 +10,21 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class LoginComponent {
 
-  meuForms!: FormGroup;
+  loginForm!: FormGroup;
 
   constructor() {
-    this.meuForms = new FormGroup({
-      nome: new FormControl('', Validators.required),
-    });
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    })
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted:', this.loginForm.value);
+    } else {
+      console.log('Form is invalid. Please check your inputs.');
+    }
   }
     
 }
