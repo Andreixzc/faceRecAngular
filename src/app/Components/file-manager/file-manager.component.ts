36d42a63bd31c0af2e8aa5db96801f57bc559c0a
Andrei <andrei.massaini@hotmail.com>
2024-01-29 04:05:49 -0300

    Rendering folders from logged user

--- src/app/Components/file-manager/file-manager.component.ts
+++ src/app/Components/file-manager/file-manager.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FolderService } from '../../Services/folder.service';
+import { FolderListResponse } from '../../Interfaces/folder-response';
 
 @Component({
   selector: 'app-file-manager',
@@ -11,18 +12,29 @@ import { FolderService } from '../../Services/folder.service';
 export class FileManagerComponent implements OnInit {
 
   constructor(private folderService: FolderService) { }
+ 
   ngOnInit(): void {
     this.renderFolders();
   }
+  folders : FolderListResponse[] = [];
   renderFolders() {
-    let jwt: string = localStorage.getItem('jwt') || '';
-    if (jwt == '') {
-      console.log('jwt is empty');
-      return;
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem('jwt', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJGYWNlUmVjIiwic3ViIjoiNzQ3MjUyMmUtMzEyMC00MjA1LWEzMWItYTA2MDA4NDMyMTA4IiwiZXhwIjoxNzA2NTE2NjI1fQ.ITLBERlkpSLZV7USu-hsS-qtySAyR5QxG-2OJIpF4Ng');
+      let jwt: string = localStorage.getItem('jwt') || '';
+      if (jwt == '') {
+        console.log('jwt is empty');
+        return;
+      }
+      this.folderService.listAllFolders(jwt).subscribe(response => {
+        this.folders = response; // Store the response data in 'folders' variable
+      });
+    } else {
+      console.log('localStorage is not available.');
     }
-    return this.folderService.listAllFolders(jwt).subscribe(response => console.log(response));
   }
-
-
-
+  
 }
+
+
+
+
