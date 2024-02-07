fe5dc738fc760873bd538880954c60fcf768d7e7
Andreixzc <andrei.massaini@hotmail.com>
2024-02-07 10:46:34 -0300

    render folder content

---
+++ src/app/Components/render-folder-content/render-folder-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-render-folder-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './render-folder-content.component.html',
+  styleUrl: './render-folder-content.component.css'
+})
+export class RenderFolderContentComponent {
+
+}
