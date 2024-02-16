d277dfc7039a183d3f9f325b4459ef825307891d
Andrei <andrei.massaini@hotmail.com>
2024-02-16 00:36:56 -0300

    dropdown button

--- src/app/Components/render-folders/render-folders.component.ts
+++ src/app/Components/render-folders/render-folders.component.ts
@@ -2,16 +2,22 @@ import { Component, Input, OnInit, input } from '@angular/core';
 import { FolderService } from '../../Services/folder.service';
 import { FolderListResponse } from '../../Interfaces/folder-response';
 import { Router } from '@angular/router';
+import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule } from '@angular/forms';
+import { event } from 'jquery';
 
 @Component({
   selector: 'app-render-folders',
   standalone: true,
-  imports: [],
+  imports: [DropdownModule,FormsModule],
   templateUrl: './render-folders.component.html',
   styleUrl: './render-folders.component.css'
 })
 export class RenderFoldersComponent  {
   
+  selectedCity: string = '';
+  cities: any[] = ['new york', 'rio de janeiro', 'paris', 'berlin', 'london', 'madrid', 'lisbon', 'amsterd'];
+  dropdownOpen: boolean = false;
   @Input() folders: FolderListResponse[] = [];
   folderIdTodisplayContent: string = ''; // Definindo a propriedade para o folderId
 
@@ -22,9 +28,17 @@ export class RenderFoldersComponent  {
     console.log(this.folderIdTodisplayContent);
     this.router.navigate(['/folder-content-page/:folderId'], { state: { folderId: this.folderIdTodisplayContent } });
   }
-  caralho(){
-    console.log("hue")
+  caralho($event : any) {
+    console.log($event);
+    const dropDown = document.getElementById('dropdownMenuButton');
+    console.log(dropDown)
+    //show dropdown:
+    dropDown?.classList.toggle('show');
+    console.log(dropDown)
   }
  
   
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,8 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS, HttpClientModule, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { loggerInterceptor } from './Auth/logger.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 
 
 
@@ -12,6 +14,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withFetch(),withInterceptors([loggerInterceptor])),
+    provideAnimations(),
   ]
 };
 
